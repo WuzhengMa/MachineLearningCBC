775e2bcb07179325000996889e2de7f95f730fed
Ma Wuzheng <oliverm0919@gmail.com>
2017-02-09 23:22:16 +0000

    error on recall/precision fixed

--- evaluation.m
+++ evaluation.m
@@ -21,7 +21,7 @@ function precision = calPR(C)
     precision = zeros(1,6);
     for i=1:6
         TP = C(i,i);
-        FP = sum(C(:,i)) - TP;
+        FP = sum(C(i,:)) - TP;
         precision(i) = TP/(TP + FP);
     end
 end
@@ -30,7 +30,7 @@ function recall = calRecall(C)
     recall = zeros(1,6);
     for i=1:6
         TP = C(i,i);
-        FN = sum(C(i,:)) - TP;
+        FN = sum(C(:,i)) - TP;
         recall(i) = TP/(TP + FN);
     end
 end
