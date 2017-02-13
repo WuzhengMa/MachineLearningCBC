68351958baf70227aa41c42993bfb1cf64c56d39
Ma Wuzheng <oliverm0919@gmail.com>
2017-02-13 10:58:27 +0000

    trainedTree.mat added

--- DecisionTreeMain.m
+++ DecisionTreeMain.m
@@ -5,14 +5,31 @@ load cleandata_students.mat;
 clean_x = x(1:1000,:);  
 clean_y = y(1:1000,:);
 
+%% Store trained tree data
+for j=1:length(y)
+    tempY(j,y(j)) = 1;
+end
+for i = 1:6
+    eachEmotionY = tempY(:,i);
+    for fold = 1:1  
+        training_data = x;
+        training_label = eachEmotionY;
+        training_size = size(training_data, 1);
+        tempTree{i, fold} = DecisionTreeLearning(training_data, 1:45, training_label);    
+    end
+end
 
+tree = tempTree(:,1);
+save('trainedTrees.mat','tree');
+
+
+%% Part II Training trees
 [numRow, numCol] = size(clean_x);
 emotionY = zeros(numRow,6);
 for j=1:length(clean_y)
     emotionY(j,clean_y(j)) = 1;
 end
 
-%% Part II Training trees
 k = 10;
 emotionTree = cell(6,10);
 C = cvpartition(zeros(1000, 1), 'KFold', k);  %perform KFold cross validation split
@@ -42,4 +59,8 @@ for fold = 1:k
     emotionLabel(:,fold) = clean_y(test(C, fold), :);
 end
 EvaluationResults = evaluation(emotionPredictions, emotionLabel)   %Get the average evaluation results for all folds
+imagesc(EvaluationResults.cmatrix);
+xlabel('Actual label');
+ylabel('Predicted label');
+title('Confusion Matrix');
 
